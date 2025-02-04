c4ddb04aad894bb5b9768039d0b1176613095aac
Danylo-Chumakov <danil26032003@gmail.com>
2025-02-04 12:39:20 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+document.addEventListener('DOMContentLoaded', () => {
+  const employeeList = document.querySelectorAll('ul > li');
+
+  function convertSalary(salary) {
+    return parseInt(salary.replace(/[^0-9.-]+/g, ''), 10);
+  }
+
+  function sortList(list) {
+    const sortedList = Array.from(list).sort((a, b) => {
+      const salaryA = convertSalary(a.dataset.salary);
+      const salaryB = convertSalary(b.dataset.salary);
+
+      return salaryB - salaryA;
+    });
+
+    const parentList = list[0].parentElement;
+
+    sortedList.forEach((item) => parentList.appendChild(item));
+  }
+
+  sortList(employeeList);
+});
